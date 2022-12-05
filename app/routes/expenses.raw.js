a11eac544ee0245109b11cad59e68057ced9ae83
Anthony Catullo <anthonycatullo@Anthonys-MacBook-Pro.local>
2022-12-05 11:38:15 -0600

    deploy commit

---
+++ app/routes/expenses.raw.js
@@ -0,0 +1,8 @@
+import { getExpenses } from "~/data/expenses.server";
+import { requireUserSession } from "~/data/auth.server";
+
+export async function loader({ request }) {
+  await requireUserSession(request);
+
+  return getExpenses();
+}
